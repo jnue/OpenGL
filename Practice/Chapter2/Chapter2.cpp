55161b467782cbee18fd10a680cc93ba8d3a79a2
jnue <wmy_june@163.com>
2024-10-21 21:04:41 +0800

    add glfw lib

--- Practice/Chapter2/Chapter2.cpp
+++ Practice/Chapter2/Chapter2.cpp
@@ -1,12 +1,38 @@
-//#include <gl/GL.h>
-//#include <gl/glew.h>
-//#include <GLFW/glfw3.h>
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 using namespace std;
 
-//void init(GLFWwindow* window) {};
+void init(GLFWwindow* window) {};
+
+void display(GLFWwindow* window, double currentTime) {
+	glClearColor(0.145, 0.627, 0.902, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
 
 int main(void) {
+	// init GLFW
+	if (!glfwInit()) exit(EXIT_FAILURE);
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	// init GLEW
+	if (glewInit() != GLEW_OK) exit(EXIT_FAILURE);
+	glfwSwapInterval(1);
+
+	init(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
